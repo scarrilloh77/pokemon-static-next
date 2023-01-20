77f6f1c73dae29339e545de3198978ac385770da
Sebastian Carrillo <sdcarrillo@uninorte.edu.co>
2023-01-19 19:34:17 -0500

    41.NextUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
+import { Button } from '@nextui-org/react';
 import { NextPage } from 'next';
 import React from 'react';
 
 const HomePage: NextPage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <div>HomePage</div>
+      <Button color="gradient">Hola mundo</Button>
+    </>
+  );
 };
 
 export default HomePage;
