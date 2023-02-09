c9cdfbaf56242a5fbb10229a8c9e4c92e7c73ae4
Sebastian Carrillo <sdcarrillo@uninorte.edu.co>
2023-02-08 22:50:06 -0500

    Navegacion y pagina de favoritos

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,6 +1,7 @@
-import { Spacer, Text, useTheme } from '@nextui-org/react';
+import { Link, Spacer, Text, useTheme } from '@nextui-org/react';
 import Image from 'next/image';
 import React from 'react';
+import NextLink from 'next/link';
 
 export const Navbar = () => {
   const { theme } = useTheme();
@@ -23,14 +24,22 @@ export const Navbar = () => {
         width={70}
         height={70}
       />
-      <Text color="white" h2>
-        P
-      </Text>
-      <Text color="white" h3>
-        okemon
-      </Text>
+      <NextLink href="/" passHref>
+        <Link>
+          <Text color="white" h2>
+            P
+          </Text>
+          <Text color="white" h3>
+            okemon
+          </Text>
+        </Link>
+      </NextLink>
       <Spacer css={{ flex: 1 }} />
-      <Text color="white">Favoritos</Text>
+      <NextLink href="/favorites" passHref>
+        <Link css={{ marginRight: '10px' }}>
+          <Text color="white">Favoritos</Text>
+        </Link>
+      </NextLink>
     </div>
   );
 };
---
+++ pages/favorites/index.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '../../components/layouts';
+
+const Favorites = () => {
+  return (
+    <Layout title="Pokemons - Favoritos">
+      <h1>Favoritos</h1>
+    </Layout>
+  );
+};
+
+export default Favorites;
